3f3ee97b2db9805feb0eb98b4c7aecfe0f3a9324
Khuchkharov25 <Khuchkharov25@gmail.com>
2024-02-29 23:38:19 +0900

    feat: create project standarts

--- src/constrollers/restaurant.controller.ts
+++ src/constrollers/restaurant.controller.ts
@@ -2,9 +2,11 @@ import {Request, Response} from 'express';
 import {T} from "../libs/types/common";
 import MemberService from "../models/Member.service"
 
+
 const restaurantController: T = {}
 restaurantController.goHome = (req:  Request, res: Response) => {
     try{
+        console.log("goHome");
         res.send("Home page");
     }catch (err) {
         console.log("Error: goHome", err);
@@ -13,6 +15,7 @@ restaurantController.goHome = (req:  Request, res: Response) => {
 
 restaurantController.getLogin = (req:  Request, res: Response) => {
     try{
+        console.log("getLogin");
         res.send("Login page");
     }catch (err) {
         console.log("Error: login", err);
@@ -21,6 +24,7 @@ restaurantController.getLogin = (req:  Request, res: Response) => {
 
 restaurantController.getSignUp = (req:  Request, res: Response) => {
     try{
+        console.log("getSignUp");
         res.send("SignUp page");
     }catch (err) {
         console.log("Error: signUp", err);
--- src/train.ts
+++ src/train.ts
@@ -1,3 +1,15 @@
+/* Project standarts
+  -Logging standarts
+  -Naming standarts
+     function, method, variable => CAMEL case   goHome
+     class => PASCAL                            MemberSevices
+     folder => KEBAB
+     Css => SNAKE                                button_style
+  -Error handling
+
+*/
+
+
 // H Task 
 
 const arr = [1, -4, 2];
