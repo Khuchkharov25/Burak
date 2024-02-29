67f44e6a8a190a3ed8c278950fd1d2680f816a22
Khuchkharov25 <Khuchkharov25@gmail.com>
2024-02-29 09:39:04 +0900

    feat: H Task Done

--- src/train.ts
+++ src/train.ts
@@ -1,1 +1,11 @@
-console.log("Hello World!");
+// H Task 
+
+const arr = [1, -4, 2];
+const arr_1 = arr.filter(getPositive);
+
+function getPositive(arr: number) {
+    return arr >= 0;
+}
+
+const javob = arr_1.join('');
+console.log(javob);
