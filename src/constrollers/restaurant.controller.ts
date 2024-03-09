b66916c8d41a8270218b9660fc0dfc76e46275f4
Khuchkharov25 <Khuchkharov25@gmail.com>
2024-03-09 15:37:11 +0900

    feat: create checkAuthSession API

--- src/constrollers/restaurant.controller.ts
+++ src/constrollers/restaurant.controller.ts
@@ -3,6 +3,7 @@ import {T} from "../libs/types/common";
 import MemberService from "../models/Member.service"
 import { AdminRequest, LoginInput, MemberInput } from '../libs/types/member';
 import { MemberType } from '../libs/enums/member.enum';
+import { Message } from '../libs/Errors';
 
 const memberService = new MemberService ();
 
@@ -61,7 +62,6 @@ restaurantController.processSignUp = async (req:  AdminRequest, res: Response) =
 restaurantController.processLogin = async (req:  AdminRequest, res: Response) => {
     try{
         console.log("processLogin");
-        console.log("body:", req.body);
         const input: LoginInput = req.body;
 
         const result = await memberService.processLogin(input);
@@ -75,6 +75,18 @@ restaurantController.processLogin = async (req:  AdminRequest, res: Response) =>
         res.send(err);
     }
 };
+
+restaurantController.checkAuthSession = async (req:  AdminRequest, res: Response) => {
+    try{
+        console.log("checkAuthSession");
+        if(req.session?.member) res.send(`<script> alert("${req.session.member.memberNick}") </script>`);
+        else res.send(`<script> alert("${Message.NOT_AUTHENTICATED}") </script>`);
+    }catch (err) {
+        console.log("Error: checkAuthSession", err);
+        res.send(err);
+    }
+};
+
 
 
 export default restaurantController;
