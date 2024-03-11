5133400ed476202abb045ea52633cdedc084b596
Khuchkharov25 <Khuchkharov25@gmail.com>
2024-03-11 08:10:05 +0500

    feat: K Task

--- src/train.ts
+++ src/train.ts
@@ -1,26 +1,50 @@
+function countVowels(str: String) {
+  const vowels = ['a', 'e', 'i', 'o', 'u'];
+  let count = 0;
+  for(let ele of str){
+    if(vowels.includes(ele)) count++;
+  }
+  return count;
+}
+
+const str = "string";
+const answer = countVowels(str);
+console.log("vowels:", answer);
+
+
+
+
+
+
+
+
+
+
+
+
 // J Task 
 
-function findLongestWord(str: string) {
+// function findLongestWord(str: string) {
  
-  const words = str.split(" ");
+//   const words = str.split(" ");
   
 
-  let longest = "";
-  let max = 0;
+//   let longest = "";
+//   let max = 0;
   
  
-  for (let i = 0; i < words.length; i++) {
-      const word = words[i];
-      if (word.length > max) {
-          longest = word;
-          max = word.length;
-      }
-  }
-  return longest;
-}
-const str = "I come from Uzbekistan";
-const longestWord = findLongestWord(str);
-console.log("The Longest word is:", longestWord);
+//   for (let i = 0; i < words.length; i++) {
+//       const word = words[i];
+//       if (word.length > max) {
+//           longest = word;
+//           max = word.length;
+//       }
+//   }
+//   return longest;
+// }
+// const str = "I come from Uzbekistan";
+// const longestWord = findLongestWord(str);
+// console.log("The Longest word is:", longestWord);
 
 
 
